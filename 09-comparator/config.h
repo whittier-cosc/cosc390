914ec4605285577841ad84b770a56dd4b1770c4d
Jeff Lutgen <jlutgen@gmail.com>
2021-08-16 14:09:37 -0700

    minor stuff

--- 09-comparator/config.h
+++ 09-comparator/config.h
@@ -49,6 +49,7 @@
 #define _SUPPRESS_PLIB_WARNING 
 #define _DISABLE_OPENADC10_CONFIGPORT_WARNING
 #include <plib.h>
+#include <peripheral/pps.h>  // Why do I need this on Mac but not Windows??
 #include "init.h"
 
 #endif
