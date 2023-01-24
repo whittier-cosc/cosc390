7c3f4ca4141a6be832d219f5d94b8fb9d0348847
Jeff Lutgen <jlutgen@gmail.com>
2023-01-24 11:12:48 -0800

    Makefile

---
+++ in-out/in-out.c
@@ -0,0 +1,21 @@
+#include "config.h"
+
+int main(void) {
+    // Configure the device for maximum performance.
+    SYSTEMConfig(SYSCLK, SYS_CFG_WAIT_STATES | SYS_CFG_PCACHE);
+    wclib_init(SYSCLK, PBCLK);
+
+    // B2: digital input
+    TRISBbits.TRISB2 = 1; // input
+    ANSELBbits.ANSB2 = 0; // digital, not analog
+    CNPUBbits.CNPUB2 = 1; // enable internal pull-up
+
+    // B3: digital output
+    TRISBbits.TRISB3 = 0; // output
+
+    while(1) {
+        // Read input, write value to output
+        LATBbits.LATB3 = PORTBbits.RB2;
+    }
+    return 0;
+}
