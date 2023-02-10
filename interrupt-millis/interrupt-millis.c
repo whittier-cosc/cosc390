6ec068d6608f21009bf367308784bb9e87adb69d
Jeff Lutgen <jlutgen@gmail.com>
2023-02-10 11:10:28 -0800

    millis

--- interrupt-millis/interrupt-millis.c
+++ interrupt-millis/interrupt-millis.c
@@ -19,11 +19,11 @@ void __ISR(_TIMER_1_VECTOR, IPL2SOFT) timer1_task() {
         g_do_second = true;
     }
 
-    // Clear the Timer1 interrupt flag
-    IFS0bits.T1IF = 0;
+    // Clear the Timer1 interrupt flag (non-PLIB version)
+    // IFS0bits.T1IF = 0;
 
-    // Clear the Timer1 interrupt flag (PLIB)
-    // INTClearFlag(INT_T1);
+    // Clear the Timer1 interrupt flag (PLIB version)
+    INTClearFlag(INT_T1);
 }
 
 int main(void) {
@@ -38,33 +38,33 @@ int main(void) {
 
     /*********************************************************/
 
-    // Configure Timer1
-    T1CONbits.TCKPS = 0; // Timer1 prescaler N = 1
-    PR1 = 39999;         // For 1 kHz interrupt frequency
-    T1CONbits.ON = 1;    // Turn on Timer1
+    // Configure Timer1 (non-PLIB version)
+    // T1CONbits.TCKPS = 0; // Timer1 prescaler N = 1
+    // PR1 = 39999;         // For 1 kHz interrupt frequency
+    // T1CONbits.ON = 1;    // Turn on Timer1
 
-    // Configure Timer1 (PLIB)
-    // OpenTimer1(T1_ON | T1_PS_1_1, 39999);
+    // Configure Timer1 (PLIB version)
+    OpenTimer1(T1_ON | T1_PS_1_1, 39999);
 
     /*********************************************************/
 
-    // Configure Timer1 interrupts
-    IEC0bits.T1IE = 1;   // Enable Timer1 interrupts
-    IPC1bits.T1IP = 2;   // Set interrupt priority level
+    // Configure Timer1 interrupts (non-PLIB version)
+    // IEC0bits.T1IE = 1;   // Enable Timer1 interrupts
+    // IPC1bits.T1IP = 2;   // Set interrupt priority level
 
-    // Configure Timer1 interrupts (PLIB)
-    // ConfigIntTimer1(T1_INT_ON | T1_INT_PRIOR_2);
+    // Configure Timer1 interrupts (PLIB version)
+    ConfigIntTimer1(T1_INT_ON | T1_INT_PRIOR_2);
 
     /*********************************************************/
 
     // Enable multi-vector interrupt mode and turn on interrupts
-    // system-wide
-    INTCONbits.MVEC = 1;
-    __builtin_enable_interrupts();
+    // system-wide (non-PLIB version)
+    // INTCONbits.MVEC = 1;
+    // __builtin_enable_interrupts();
 
     // Enable multi-vector interrupt mode and turn on interrupts
-    // system-wide (PLIB)
-    // INTEnableSystemMultiVectoredInt();
+    // system-wide (PLIB version)
+    INTEnableSystemMultiVectoredInt();
 
     /*********************************************************/
 
